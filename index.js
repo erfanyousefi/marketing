1a3cb573a0b70f8fdf9bc8a4fa2c9c89bc5285bd
reza4096 <reza4096@yahoo.com>
2021-09-23 12:23:33 +0000

    index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Marketing Project')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`listening at http://localhost:${port}`)
 })
